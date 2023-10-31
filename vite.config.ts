0d2906c3a2e076d96e72b93a2ca51b2a4ee0143f
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2023-10-31 16:55:40 +0100

    inject css in the js bundle
    Without this, when building, Vite would generate a style.css file that would need to be imported by users in the host app, which is not an ideal DX
    
    See https://github.com/vitejs/vite/issues/1579

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vitest/config'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react-swc'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({
-    insertTypesEntry: true
-  })],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    cssInjectedByJsPlugin(),
+  ],
 
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'CAP Moses',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "CAP Moses",
       // the proper extensions will be added
-      fileName: 'cap-moses',
+      fileName: "cap-moses",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-redux', 'redux'],
+      external: ["react"],
     },
   },
 
@@ -28,4 +33,4 @@ export default defineConfig({
     globals: true,
     environment: "happy-dom",
   },
-})
+});
