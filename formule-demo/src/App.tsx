94a05dcea7054ab658b6c865d64bc4ea229231c5
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2024-02-22 14:02:04 +0100

    fix demo layout, add demo to readme

--- formule-demo/src/App.tsx
+++ formule-demo/src/App.tsx
@@ -3,11 +3,12 @@ import { SchemaPreview } from "react-formule";
 import { FormPreview } from "react-formule";
 import { initFormuleSchema } from "react-formule";
 import { useEffect } from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Layout, Space, Typography } from "antd";
+import { theme } from "./theme";
 
-import "./style.css"
+import "./style.css";
 
-const PRIMARY_COLOR = "#006996";
+const { Content, Footer } = Layout;
 
 function App() {
   useEffect(() => {
@@ -15,52 +16,64 @@ function App() {
   }, []);
 
   return (
-    <FormuleContext theme={{
-      token: {
-        colorPrimary: PRIMARY_COLOR,
-        colorLink: PRIMARY_COLOR,
-        colorLinkHover: "#1a7fa3",
-        borderRadius: 2,
-        colorBgLayout: "#f0f2f5",
-        fontFamily: "Titillium Web",
-      },
-    }}>
-      <Row style={{ height: "100%" }}>
-        <Col
-          xs={10}
-          sm={5}
-          style={{
-            overflowX: "hidden",
-            height: "100%",
-            display: "flex",
-          }}
-          className="tour-field-types"
+    <Layout style={{ height: "100%" }}>
+      <Content>
+        <FormuleContext theme={theme}>
+          <Row style={{ height: "100%" }}>
+            <Col
+              xs={10}
+              sm={5}
+              style={{
+                overflowX: "hidden",
+                height: "100%",
+                display: "flex",
+              }}
+              className="tour-field-types"
+            >
+              <SelectOrEdit />
+            </Col>
+            <Col
+              xs={14}
+              sm={5}
+              style={{
+                overflowX: "hidden",
+                padding: "0px 15px",
+                backgroundColor: "#F6F7F8",
+              }}
+              className="tour-schema-preview"
+            >
+              <SchemaPreview />
+            </Col>
+            <Col
+              xs={24}
+              sm={14}
+              style={{
+                overflowX: "hidden",
+                height: "100%",
+                padding: "0px 15px",
+              }}
+              className="tour-form-preview"
+            >
+              <FormPreview liveValidate={true} />
+            </Col>
+          </Row>
+        </FormuleContext>
+      </Content>
+      <Footer style={{ padding: 0 }}>
+        <Row
+          align="bottom"
+          justify="center"
+          style={{ padding: "5px", background: "#001529" }}
         >
-          <SelectOrEdit />
-        </Col>
-        <Col
-          xs={14}
-          sm={5}
-          style={{
-            overflowX: "hidden",
-            padding: "0px 15px",
-            backgroundColor: "#F6F7F8",
-          }}
-          className="tour-schema-preview"
-        >
-          <SchemaPreview />
-        </Col>
-        <Col
-          xs={24}
-          sm={14}
-          style={{ overflowX: "hidden", height: "100%", padding: "0px 15px" }}
-          className="tour-form-preview"
-        >
-          <FormPreview liveValidate={true} />
-        </Col>
-      </Row>
-      
-    </FormuleContext>
+          <Space direction="horizontal" size="middle">
+            <Typography.Text style={{ color: "rgba(255, 255, 255, 0.65)" }}>
+              Running react-formule v{import.meta.env.REACT_FORMULE_VERSION}{" "}
+              deployed from master
+            </Typography.Text>
+          </Space>
+        </Row>
+      </Footer>
+    </Layout>
   );
 }
 
--- formule-demo/src/main.tsx
+++ formule-demo/src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ConfigProvider } from "antd";
+import { theme } from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={theme}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
-)
+);
