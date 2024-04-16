5c72b597fc89f6062fe35ca35b96b777751ff009
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2024-04-16 15:49:13 +0200

    fix(actions): change semantic-release branches to master

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 export default {
-  branches: ["add-semantic-release"],
+  branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
