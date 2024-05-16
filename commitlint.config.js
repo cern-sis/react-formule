5cbd975a0006c0e1c2db218440e813e9b1d4d68f
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2024-05-16 15:32:18 +0200

    ci(publish): tweak semantic-release config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,5 @@
-import * as rules from "@commitlint/rules";
-
 export default {
   extends: ["@commitlint/config-conventional"],
-  plugins: [
-    {
-      rules: {
-        // Excludes markdown links from body max length calculation
-        // (needed for semantic-release automatic commits)
-        "body-max-length": (parsed, _when, _value) => {
-          parsed.header = parsed.header.replace(/\[([^[\]]+)\]\(([^)]+)\)/, "");
-          return rules.default["body-max-length"](parsed, _when, _value);
-        },
-      },
-    },
-  ],
+  // Skip for semantic-release automatic commits
+  ignores: [(message) => /^chore\(release\):.*\[skip ci\]/.test(message)],
 };
--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 export default {
-  branches: ["master", "semantic-release-github-plugin"],
+  branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
@@ -7,7 +7,7 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "CHANGELOG.md"],
+        assets: ["package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
