207df6330678afe07336269daeba224b02323214
Pamfilos Fokianos <pamfilosf@gmail.com>
2025-12-01 11:35:17 +0100

    feat(widgets): switch - update for customizing checked/uncheked labels

--- src/forms/widgets/base/SwitchWidget.jsx
+++ src/forms/widgets/base/SwitchWidget.jsx
@@ -12,8 +12,10 @@ const SwitchWidget = ({
   readonly,
   value,
   schema,
+  options,
 }) => {
   const { readonlyAsDisabled = true } = formContext;
+  const { checkedLabel, uncheckedLabel, bgColor } = options;
 
   const handleChange = (checked) => {
     if (schema.type === "string") {
@@ -29,13 +31,20 @@ const SwitchWidget = ({
 
   const handleFocus = ({ target }) => onFocus(id, target.checked);
 
+  const isChecked = schema.type === "string" ? value === "true" : value;
   return (
     <Switch
+      style={{
+        background: bgColor,
+        opacity: isChecked ? 1 : 0.5,
+      }}
       autoFocus={autofocus}
       checked={schema.type === "string" ? value === "true" : value}
       disabled={disabled || (readonlyAsDisabled && readonly)}
       id={id}
       name={id}
+      checkedChildren={checkedLabel}
+      unCheckedChildren={uncheckedLabel}
       onBlur={!readonly ? handleBlur : undefined}
       onChange={!readonly ? handleChange : undefined}
       onFocus={!readonly ? handleFocus : undefined}
